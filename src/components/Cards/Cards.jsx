15c15fb1c238c5613a9b98929f2ab93b5b14b64c
Shaeen <shaeenkevinsingh@gmail.com>
2020-05-18 19:25:54 +0200

    Destructuring data and displaying in the view

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 
-const Cards = (props) => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading... ";
+  }
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -11,7 +15,7 @@ const Cards = (props) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">Real Data</Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
             <Typography color="textSecondary">Real date</Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
